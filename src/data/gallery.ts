38a20ae7e4952f2dccabd740cd425caf43a44782
jrodolfovillasenorm <jrodolfovillasenor@gmail.com>
2026-09-25 13:50:51 -0600

    actualización galería

--- src/data/gallery.ts
+++ src/data/gallery.ts
@@ -5,93 +5,94 @@ export interface GalleryItem {
 
 export const gallery: GalleryItem[] = [
   {
-    image: "/photos/gallery/image1.png",
-    caption: "SEGUNDO PISO LIVE",
+    image: "/photos/gallery/image7.jpeg",
+    caption: "VENENOISE",
   },
   {
-    image: "/photos/gallery/image2.png",
-    caption: "SPYDERS",
+    image: "/photos/gallery/image8.jpeg",
+    caption: "CORVO",
   },
   {
     image: "/photos/gallery/image4.png",
     caption: "ALBUM COVER",
   },
   {
-    image: "/photos/gallery/image5.png",
-    caption: "PINS",
+    image: "/photos/gallery/pic1.jpg",
+    caption: "???",
   },
   {
     image: "/photos/gallery/image3.png",
     caption: "CAPITAN GALLO",
   },
   {
-    image: "/photos/gallery/image1.png",
+    image: "/photos/gallery/image6.jpg",
     caption: "SEGUNDO PISO LIVE",
   },
   {
-    image: "/photos/gallery/image2.png",
+    image: "/photos/gallery/image9.jpg",
+    caption: "CLOSE UP",
+  },
+  {
+    image: "/photos/gallery/image10.jpeg",
+    caption: "MIXCOAC53",
+  },
+  {
+    image: "/photos/gallery/image5.png",
+    caption: "PINS",
+  },
+  {
+    image: "/photos/gallery/image11.jpeg",
+    caption: "CORVO",
+  },
+  {
+    image: "/photos/gallery/guitar.jpeg",
+    caption: "GUITAR",
+  },
+  {
+    image: "/photos/gallery/image12.jpeg",
+    caption: "READY",
+  },
+  {
+    image: "/photos/gallery/image13.jpeg",
+    caption: "VENAS ROTAS",
+  },
+  {
+    image: "/photos/gallery/image14.jpg",
     caption: "SPYDERS",
   },
   {
-    image: "/photos/gallery/image4.png",
-    caption: "ALBUM COVER",
+    image: "/photos/gallery/image15.jpg",
+    caption: "UNDER",
   },
   {
-    image: "/photos/gallery/image5.png",
-    caption: "PINS",
+    image: "/photos/gallery/image16.jpg",
+    caption: "MUERTHO",
   },
   {
-    image: "/photos/gallery/image3.png",
-    caption: "CAPITAN GALLO",
+    image: "/photos/gallery/image17.jpeg",
+    caption: "DOBBERMAN",
   },
   {
-    image: "/photos/gallery/image2.png",
-    caption: "SPYDERS",
+    image: "/photos/gallery/image18.jpeg",
+    caption: "STICKERS",
   },
   {
-    image: "/photos/gallery/image4.png",
-    caption: "ALBUM COVER",
+    image: "/photos/gallery/picture3.jpg",
+    caption: "NEBULAR",
   },
   {
-    image: "/photos/gallery/image5.png",
-    caption: "PINS",
+    image: "/photos/gallery/spider.png",
+    caption: "ARAÑA",
   },
   {
-    image: "/photos/gallery/image3.png",
-    caption: "CAPITAN GALLO",
+    image: "/photos/gallery/lerat.jpeg",
+    caption: "LE RAT",
   },
   {
-    image: "/photos/gallery/image1.png",
-    caption: "SEGUNDO PISO LIVE",
+    image: "/photos/gallery/sound.jpeg",
+    caption: "SOUND",
   },
-  {
-    image: "/photos/gallery/image2.png",
-    caption: "SPYDERS",
-  },
-  {
-    image: "/photos/gallery/image4.png",
-    caption: "ALBUM COVER",
-  },
-  {
-    image: "/photos/gallery/image5.png",
-    caption: "PINS",
-  },
-  {
-    image: "/photos/gallery/image3.png",
-    caption: "CAPITAN GALLO",
-  },
-  {
-    image: "/photos/gallery/image1.png",
-    caption: "SEGUNDO PISO LIVE",
-  },
-  {
-    image: "/photos/gallery/image4.png",
-    caption: "ALBUM COVER",
-  },
-  {
-    image: "/photos/gallery/image5.png",
-    caption: "PINS",
-  },
+
   {
     image: "/photos/gallery/image3.png",
     caption: "CAPITAN GALLO",
